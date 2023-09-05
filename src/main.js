efa0dffd2c790e9a5583c3cf2de10a61f1f8aa50
João pedro Maciel pinheiro <shionlk98@gmail.com>
2023-09-05 12:13:35 -0300

    last  update

--- src/main.js
+++ src/main.js
@@ -1,25 +1,22 @@
 lucide.createIcons();
 
-let select = document.querySelector('.select');
-let selectdValue = document.getElementById('selected-value');
-let optionsViewButton = document.getElementById('options-view-button');
-let inputsOptions = document.querySelectorAll('.option input');
+let select = document.querySelector(".select"),
+  selectdValue = document.getElementById("selected-value"),
+  optionsViewButton = document.getElementById("options-view-button"),
+  inputsOptions = document.querySelectorAll(".option input");
 
-inputsOptions.forEach(input => {
-  input.addEventListener('click', event => {
-    selectdValue.textContent = input.dataset.label
-  })
-})
+inputsOptions.forEach((input) => {
+  input.addEventListener("click", (event) => {
+    selectdValue.textContent = input.dataset.label;
+
+    const isMouseOrTouch =
+      event.pointerType == "mouse" || event.pointerType == "touch";
+
+    isMouseOrTouch && optionsViewButton.click();
+  });
+});
 
 // let menu = document.querySelector('#options');
 // function openmenu() {
 //     var element = document.querySelector("#options");
 //     element.classList.toggle("options-2");}
-
-
-
-
-// const isMouseOrTouch =
-// event.pointerType == "mouse" || event.pointerType == "touch";
-
-// isMouseOrTouch && optionsViewButton.click();
